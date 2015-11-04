f24de043d9a923f240b94882f81889e76fcf9a54
Andre Ogle <ogleandre@yahoo.com>
2015-11-04 16:35:18 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,54 @@
+# eslint-teamcity
+
+> A small [eslint](https://github.com/eslint/eslint) formatter plugin.
+ESLint violations are output nicely in the
+[TeamCity](https://www.jetbrains.com/teamcity/) build error format.
+
+## Installation
+
+[Use npm](https://docs.npmjs.com/cli/install).
+
+```sh
+npm install eslint-teamcity --save-dev
+```
+
+## Usage
+```sh
+  eslint --format './node_modules/eslint-teamcity/index.js' myfiletolint.js
+```
+
+## [gulp-eslint](https://github.com/adametry/gulp-eslint) integration
+```javascript
+var gulp = require('gulp'),
+    eslint = require('gulp-eslint'),
+    teamcity = require('eslint-teamcity');
+
+gulp.task('lint', function () {
+    return gulp.src(['js/**/*.js'])
+        .pipe(eslint())
+        .pipe(eslint.format(teamcity))
+        .pipe(eslint.failAfterError());
+});
+```
+See the [gulp-eslint](https://github.com/adametry/gulp-eslint#usage) docs for
+more info on setting up a linting task.
+
+
+## TeamCity usage (with [gulp-eslint](http://github.com/adametry/gulp-eslint))
+Add a gulp task to run ESLint (see above)
+
+Setup a TeamCity build step, similar to the below screenshot:
+
+![Example TeamCity Setup](https://i.imgur.com/j7qMSYg.jpg)
+
+Kick off a new build, by deploying again, and you should see your build errors - assuming you have any!
+
+#### Screenshot with TeamCity
+
+![Example TeamCity Output](https://i.imgur.com/DkwEPEN.jpg)
+
+## Issues
+
+I will try keep this project up to date, but please log any issues
+[here](https://github.com/andreogle/eslint-teamcity/issues).
+Any pull requests are also welcome!
---
+++ src/formatter.js
@@ -0,0 +1,77 @@
+/**
+ * @fileoverview Teamcity report formatter plugin for ESLint
+ * @author Andre Ogle
+ */
+
+'use strict';
+
+//------------------------------------------------------------------------------
+// Helpers
+//------------------------------------------------------------------------------
+
+/**
+ * Escape special characters with the respective TeamCity escaping.
+ * @param {string} str An error message to display in TeamCity.
+ * @returns {string} An error message formatted for display in TeamCity
+ */
+function escapeTeamCityString(str) {
+  if (!str) {
+    return '';
+  }
+
+  return str.replace(/\|/g, '||')
+    .replace(/\'/g, '|\'')
+    .replace(/\n/g, '|n')
+    .replace(/\r/g, '|r')
+    .replace(/\u0085/g, '|x')
+    .replace(/\u2028/g, '|l')
+    .replace(/\u2029/g, '|p')
+    .replace(/\[/g, '|[')
+    .replace(/\]/g, '|]');
+}
+
+var reportName = 'ESLint Violations';
+
+//------------------------------------------------------------------------------
+// Public Interface
+//------------------------------------------------------------------------------
+module.exports = function(results) {
+  var output = '';
+
+  output += '##teamcity[testSuiteStarted name=\'' + reportName + '\']\n';
+
+  results.forEach(function(result) {
+    var messages = result.messages;
+
+    if (messages.length === 0) {
+      return;
+    }
+
+    output += '##teamcity[testStarted name=\'' + reportName + ': ' +
+               escapeTeamCityString(result.filePath) + '\']\n';
+
+    var messageList = [];
+
+    messages.forEach(function(message) {
+      if (message.severity === 2) {
+        messageList.push(
+          'line ' + message.line +
+          ', col ' + message.column + ', ' + message.message
+        );
+      }
+    });
+
+    if (messageList.length) {
+      output += '##teamcity[testFailed name=\'' + reportName + ': ' +
+        escapeTeamCityString(result.filePath) + '\' message=\'' +
+        escapeTeamCityString(messageList.join('\n')) + '\']\n';
+    }
+
+    output += '##teamcity[testFinished name=\'' + reportName + ': ' +
+      escapeTeamCityString(result.filePath) + '\']\n';
+  });
+
+  output += '##teamcity[testSuiteFinished name=\'' + reportName + '\']\n';
+
+  return output;
+};
