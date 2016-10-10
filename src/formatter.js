cefa8f155da8353c5f2a01e2fc29c6956067f0a1
Anton Fedchenko <kompotik@gmail.com>
2016-10-10 02:28:08 +0200

    Add summary statistics.

--- src/formatter.js
+++ src/formatter.js
@@ -5,6 +5,9 @@
 
 'use strict';
 
+var fs = require('fs');
+var path = require('path');
+
 //------------------------------------------------------------------------------
 // Helpers
 //------------------------------------------------------------------------------
@@ -34,13 +37,40 @@ function escapeTeamCityString(str) {
 
 var reportName = 'ESLint Violations';
 
+var options = {
+  details: {
+    default: true
+  },
+  summary: {
+    default: false
+  }
+};
+
+var config = JSON.parse(fs.readFileSync('package.json'))['eslint-teamcity'] || {};
+
+function getOptionValue(optionName) {
+  if (optionName in config) {
+    return config[optionName];
+  } else {
+    return options[optionName].default;
+  }
+}
+
 //------------------------------------------------------------------------------
 // Public Interface
 //------------------------------------------------------------------------------
 module.exports = function(results) {
-  var output = '';
 
-  output += '##teamcity[testSuiteStarted name=\'' + reportName + '\']\n';
+  var optionDetails = getOptionValue('details');
+  var optionSummary = getOptionValue('summary');
+
+  var output = '';
+  var errorCount = 0;
+  var warningCount = 0;
+
+  if (optionDetails) {
+    output += '##teamcity[testSuiteStarted name=\'' + reportName + '\']\n';
+  }
 
   results.forEach(function(result) {
     var messages = result.messages;
@@ -49,8 +79,10 @@ module.exports = function(results) {
       return;
     }
 
-    output += '##teamcity[testStarted name=\'' + reportName + ': ' +
-               escapeTeamCityString(result.filePath) + '\']\n';
+    if (optionDetails) {
+      output += '##teamcity[testStarted name=\'' + reportName + ': ' +
+        escapeTeamCityString(result.filePath) + '\']\n';
+    }
 
     var errorsList = [];
     var warningsList = [];
@@ -61,26 +93,40 @@ module.exports = function(results) {
 
       if (message.fatal || message.severity === 2) {
         errorsList.push(userMessage);
+        errorCount++;
       } else {
         warningsList.push(userMessage);
+        warningCount++;
       }
     });
 
-    if (errorsList.length) {
-      output += '##teamcity[testFailed name=\'' + reportName + ': ' +
-        escapeTeamCityString(result.filePath) + '\' message=\'' +
-        escapeTeamCityString(errorsList.join('\n')) + '\']\n';
-    } else if (warningsList.length) {
-      output += '##teamcity[testStdOut name=\'' + reportName + ': ' +
-        escapeTeamCityString(result.filePath) + '\' out=\'warning: ' +
-        escapeTeamCityString(warningsList.join('\n')) + '\']\n';
+    if (optionDetails) {
+      if (errorsList.length) {
+        output += '##teamcity[testFailed name=\'' + reportName + ': ' +
+          escapeTeamCityString(result.filePath) + '\' message=\'' +
+          escapeTeamCityString(errorsList.join('\n')) + '\']\n';
+      } else if (warningsList.length) {
+        output += '##teamcity[testStdOut name=\'' + reportName + ': ' +
+          escapeTeamCityString(result.filePath) + '\' out=\'warning: ' +
+          escapeTeamCityString(warningsList.join('\n')) + '\']\n';
+      }
+      output += '##teamcity[testFinished name=\'' + reportName + ': ' +
+        escapeTeamCityString(result.filePath) + '\']\n';
     }
-
-    output += '##teamcity[testFinished name=\'' + reportName + ': ' +
-      escapeTeamCityString(result.filePath) + '\']\n';
   });
 
-  output += '##teamcity[testSuiteFinished name=\'' + reportName + '\']\n';
+  if (optionDetails) {
+    output += '##teamcity[testSuiteFinished name=\'' + reportName + '\']\n';
+  }
+
+  if (optionSummary) {
+    if (errorCount !== 0) {
+      output += '##teamcity[buildStatisticValue key=\'ESLintErrorCount\' value=\'' + errorCount +'\' ]\n';
+    }
+    if (warningCount !== 0) {
+      output += '##teamcity[buildStatisticValue key=\'ESLintWarningCount\' value=\'' + warningCount +'\' ]\n';
+    }
+  }
 
   return output;
 };
