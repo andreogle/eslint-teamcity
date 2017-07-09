ecf8e086f42fcdfd245eac89abbe5286c81e33fa
akaguny <alexeykolpino@gmail.com>
2017-07-09 18:22:30 +0300

    feat(*): add cmd interface support

--- index.js
+++ index.js
@@ -1,3 +1,15 @@
-'use strict';
+#!/usr/bin/env node
 
-module.exports = require('./src/formatter');
+'use strict';
+var fs = require('fs-extra'),
+  procArg = process.argv;
+
+if (require.main === module) {
+  process.stdout.write(main(fs.readJSONSync(procArg[2])))
+} else {
+  module.exports = main;
+}
+
+function main(input) {
+  return require('./src/formatter')(input);
+}
