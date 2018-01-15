15fc0d39771d105e390320975800c3637dd8eb98
Andre Ogle <ogleandre@gmail.com>
2018-01-15 13:48:06 +0200

    Add back smoke tests

--- test/smoke.spec.js
+++ test/smoke.spec.js
@@ -1,52 +1,58 @@
-// var expect = require('chai').expect;
-// var sinon = require('sinon');
-// var sh = require('shelljs');
-// var path = require('path');
-// var fs = require('fs-extra');
-// var basePath = path.resolve(__dirname, '..');
-// var eslintResultGenerator = require('./helpers/eslint-result-generator');
-// var pathToTestJson = path.resolve(__dirname, 'result.json');
-// var pathToIndex = path.resolve(__dirname, '..', 'index.js');
-//
-// var input = [];
-// var result;
-// describe('support interface',function() {
-//   before(function() {
-//     input.push(eslintResultGenerator.createDummyError());
-//   });
-//
-//   describe('cmd',function() {
-//     it('as eslint formatter plugin', function() {
-//       result = sh.exec('eslint --format ' + '\'' + pathToIndex + '\' ' + pathToIndex);
-//       expect(result.stdout).to.contain('##teamcity');
-//     });
-//
-//     it('as standalone',function() {
-//       fs.writeJSONSync(pathToTestJson, input);
-//       result = sh.exec('cd ' + basePath + '; node ' + 'index.js ' + pathToTestJson);
-//       expect(result.stdout).to.contain('##teamcity');
-//       expect(result.stderr).to.equal('');
-//
-//       sh.rm(pathToTestJson);
-//     });
-//   });
-//
-//   describe('requirejs',function() {
-//     it('basic', function() {
-//       result = require(pathToIndex)(input);
-//       expect(result).to.contain('##teamcity');
-//     });
-//
-//     it('with parameters', function() {
-//       var teamcityPropNames = {
-//         errorCountName: 'EslintInspectionStatsE',
-//         warningCountName: 'EslintInspectionStatsW'
-//       };
-//
-//       result = require(pathToIndex)(input, teamcityPropNames);
-//       expect(result).to.contain('ESLint Violations');
-//       expect(result).to.contain('EslintInspectionStatsE');
-//       expect(result).to.contain('EslintInspectionStatsW');
-//     });
-//   });
-// });
+/* global it, describe, beforeEach, afterEach */
+
+const { expect } = require('chai');
+const sh = require('shelljs');
+const path = require('path');
+const fs = require('fs-extra');
+const { createDummyError } = require('./helpers/eslint-result-generator');
+
+const basePath = path.resolve(__dirname, '..');
+const pathToTestJson = path.resolve(__dirname, 'result.json');
+const pathToIndex = path.resolve(__dirname, '..', 'index.js');
+
+describe('support interface', () => {
+  let esLintOutput = [];
+
+  beforeEach(() => {
+    esLintOutput.push(createDummyError());
+  });
+
+  afterEach(() => {
+    esLintOutput = [];
+  });
+
+  describe('cmd', () => {
+    it('as eslint formatter plugin', () => {
+      const result = sh.exec(`eslint --format '${pathToIndex}' ${pathToIndex}`);
+      expect(result.stdout).to.contain('##teamcity');
+    });
+
+    it('as standalone', () => {
+      fs.writeJSONSync(pathToTestJson, esLintOutput);
+      const result = sh.exec(`cd ${basePath}; node index.js ${pathToTestJson}`);
+      expect(result.stdout).to.contain('##teamcity');
+      expect(result.stderr).to.equal('');
+
+      sh.rm(pathToTestJson);
+    });
+  });
+
+  describe('requirejs', () => {
+    it('basic', () => {
+      const result = require(pathToIndex)(esLintOutput);
+      expect(result).to.contain('##teamcity');
+    });
+
+    it('with parameters', () => {
+      const teamcityPropNames = {
+        errorCountName: 'EslintInspectionStatsE',
+        warningCountName: 'EslintInspectionStatsW'
+      };
+
+      const result = require(pathToIndex)(esLintOutput, teamcityPropNames);
+      expect(result).to.contain('ESLint Violations');
+      expect(result).to.contain('EslintInspectionStatsE');
+      expect(result).to.contain('EslintInspectionStatsW');
+    });
+  });
+});
