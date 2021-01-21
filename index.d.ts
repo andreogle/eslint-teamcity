6c57bd5c046b10202591506cc8e425c3cd1e948e
Idan Attias <idana@wix.com>
2021-01-21 11:13:06 +0200

    update

--- index.d.ts
+++ index.d.ts
@@ -1,9 +1,5 @@
 import { ESLint } from "eslint";
 
-declare namespace ESLintTeamCityReporter {
-  function format(results: ESLint.LintResult[], { reportName }: { reportName?: string }): string;
+declare function format(results: ESLint.LintResult[], { reportName }: { reportName?: string }): string;
 
-  export default format;
-}
-
-export = ESLintTeamCityReporter;
+export = format;
